74db7bb6c258440bdca972dcb31f3e3b122ede38
Madjakul <madjakul@protonmail.com>
2023-02-08 12:22:15 +0100

    Correcting the installation script.

--- install_config.sh
+++ install_config.sh
@@ -15,21 +15,22 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 
 # Installing the Python's dependencies
 echo ${green}=== Installing Python\'s Dependencies ===${reset}
-pip3 install --upgrade -r ./requirements.txt
+pip3 install --upgrade -r requirements.txt
 
 # Installing the correct font
 echo ${green}=== Installing the Roboto Font ===${reset}
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/RobotoMono.zip
 mkdir roboto_font
 unzip ./RobotoMono.zip -d ./roboto_font
-rm -rf ./roboto_font
 # Moving the font to the .fonts directory
 if [ -d "~/.fonts" ]; # If the directory ~/.fonts already exist
 then
     mv -v ./roboto_font/* ~/.fonts
+    rm -rf ./roboto_font
 else
     mkdir ~/.fonts
     mv -v ./roboto_font/* ~/.fonts
+    rm -rf ./roboto_font
 fi
 cd ~/.fonts
 fc-cache -fv
